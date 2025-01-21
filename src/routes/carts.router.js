323cc0b1bed883cc47e3c4e5007fcddda0216736
franco de gaetani <francodegaetani@gmail.com>
2025-01-21 18:15:54 -0300

    second  commit

--- src/routes/carts.router.js
+++ src/routes/carts.router.js
@@ -1,14 +1,45 @@
 import { Router } from "express";
 
 const router = Router()
-let products = []
 
+let carritos = []
+
+let productos = [
+    { id: '001', name: 'Hamburguesa' },
+    { id: '002', name: 'Papas fritas' },
+    { id: '003', name: 'Refresco' },
+];
+
+
+router.get('/', (req,res)=>{
+
+    res.json({carritos})
+})
 
 router.post('/',(req, res)=>{
 
-    
+    const nuevoCarrito = {
+        id :(carritos.length + 1).toString(),
+        products : []
+    }
 
+    carritos.push(nuevoCarrito);
+    res.json(carritos);
 })
+
+router.get('/:id', (req,res)=>{
+
+    const carritoId = req.params.id;
+
+    const carritoEncontrado = carritos.find((e) => e.id === carritoId);
+
+    if(!carritoEncontrado){
+        return res.status(404).json({error : 'Carritos no encontrado'});
+    }
+
+    res.json({carritoEncontrado});
+})
+
 
 
 
